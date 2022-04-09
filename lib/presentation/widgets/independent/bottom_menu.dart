4784e4b0cfdd71d5a7d01f46b146b872e5921735
enpartha <enpartha@gmail.com>
2022-04-09 08:47:29 +0530

    migrate to null safety

--- lib/presentation/widgets/independent/bottom_menu.dart
+++ lib/presentation/widgets/independent/bottom_menu.dart
@@ -4,11 +4,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:openflutterecommerce/config/app_settings.dart';
+import '../config/app_settings.dart';
 import 'package:openflutterecommerce/config/routes.dart';
 
 class OpenFlutterBottomMenu extends StatelessWidget {
-  final int menuIndex;
+  final int? menuIndex;
 
   OpenFlutterBottomMenu(this.menuIndex);
 
@@ -25,29 +25,22 @@ class OpenFlutterBottomMenu extends StatelessWidget {
         width: 24.0,
         color: colorByIndex(theme, index),
       ),
-      title: Text(
-        title,
-        style: TextStyle(
-          fontSize: 10.0,
-          color: colorByIndex(theme, index),
-        ),
-      ),
+      label: title,
     );
   }
 
   @override
   Widget build(BuildContext context) {
     final _theme = Theme.of(context);
-    List<BottomNavigationBarItem> menuItems =[
+    List<BottomNavigationBarItem> menuItems = [
       getItem('assets/icons/bottom_menu/home.svg', 'Home', _theme, 0),
       getItem('assets/icons/bottom_menu/cart.svg', 'Shop', _theme, 1),
       getItem('assets/icons/bottom_menu/bag.svg', 'Bag', _theme, 2),
-      getItem('assets/icons/bottom_menu/favorites.svg', 'Favorites',
-          _theme, 3),
+      getItem('assets/icons/bottom_menu/favorites.svg', 'Favorites', _theme, 3),
     ];
-    if ( AppSettings.profileEnabled ) {
+    if (AppSettings.profileEnabled) {
       menuItems.add(getItem(
-        'assets/icons/bottom_menu/profile.svg', 'Profile', _theme, 4));
+          'assets/icons/bottom_menu/profile.svg', 'Profile', _theme, 4));
     }
     return Container(
       decoration: BoxDecoration(
@@ -64,7 +57,7 @@ class OpenFlutterBottomMenu extends StatelessWidget {
         ),
         child: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
-          currentIndex: menuIndex,
+          currentIndex: menuIndex!,
           onTap: (value) {
             switch (value) {
               case 0:
